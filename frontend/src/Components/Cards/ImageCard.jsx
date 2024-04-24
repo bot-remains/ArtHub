0453cecc4055607ce2a26a2579e5e4ae3c2173fb
HarshPatel1100 <harshpatel120605@gmail.com>
2024-04-24 12:23:40 +0530

    Added buttons in the card of images

--- frontend/src/Components/Cards/ImageCard.jsx
+++ frontend/src/Components/Cards/ImageCard.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import {FaArrowDown, FaRegHeart, FaShare, FaPlus} from "react-icons/fa6";
 
 function ImageCard({imageUrl}) {
+  const profilePic = {
+    backgroundImage: `url("https://res.cloudinary.com/djdegiywz/image/upload/v1713686815/ke4qgpdy9yaaqyix03k1.jpg")`,
+    backgroundPosition: "top",
+    backgroundSize: "cover",
+  };
+
   return (
-    <div className="w-fit h-fit border-[10px] cursor-pointer">
+    <div className="w-fit h-fit border-[10px] cursor-pointer relative group/card">
+      <div className="w-full h-full absolute top-0 z-10 bg-black opacity-50 hidden group-hover/card:block"></div>
+      <div className="absolute w-full text-sm text-zinc-400 p-3 top-0 z-20 bg-gradient-to-b from-black hidden group-hover/card:flex justify-between ">
+        <div className="flex items-center justify-start ">
+          <div className="border-[2px] border-zinc-400 rounded-full w-fit p-[8px] flex items-center justify-center group hover:border-secondary">
+            <FaRegHeart className="group-hover:text-secondary" />
+          </div>
+        </div>
+        <div className="flex items-center justify-end gap-2">
+          <div className="border-[2px] border-zinc-400  rounded-full w-fit p-[8px] flex items-center justify-center group hover:border-sky-300">
+            <FaShare className="group-hover:text-sky-300" />
+          </div>
+          <div className="border-[2px] border-zinc-400 rounded-full w-fit p-[8px] flex items-center justify-center group hover:border-green-400">
+            <FaPlus className="group-hover:text-green-400" />
+          </div>
+        </div>
+      </div>
+      <div className="absolute w-full p-3 bottom-0 z-20 bg-gradient-to-t from-black hidden group-hover/card:flex justify-between items-center">
+        <div className="flex items-center gap-3">
+          <div
+            className="rounded-full w-[35px] h-[35px]"
+            style={profilePic}
+          ></div>
+          <div className="text">name</div>
+        </div>
+        <div className="border-[2px] text-sm text-zinc-400 border-zinc-400 rounded-full w-fit p-[8px] flex items-center justify-center group hover:border-green-400">
+          <FaArrowDown className="group-hover:text-green-400" />
+        </div>
+      </div>
       <img src={imageUrl} className="w-[400px]" />
     </div>
   );
